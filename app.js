bc9b45eba814afbb8fb89341e0b396782001d5bb
Sindhu Telikapalli <sindhutelikapalli@Sindhus-MacBook-Air.local>
2025-01-11 17:57:38 +0530

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, AWS Elastic Beanstalk!');
+    res.send('Hello, AWS Elastic Beanstalk,this is version 2!');
 });
 
 const PORT = process.env.PORT || 3000;
